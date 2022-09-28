327707e308330acd7646f961ea063f4b2e32eb24
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-09-28 15:51:43 +0400

    cleanup and some minor improvements

--- contract/Fundraiser.sol
+++ contract/Fundraiser.sol
@@ -21,7 +21,7 @@ contract Fundraiser {
         string prizeName;
         string prizeImage;
         address payable organizer;
-        bool status;
+        bool active;
         uint amountToBeRaised;
         uint numberOfContributions;
         uint256 amountRaised;
@@ -29,10 +29,9 @@ contract Fundraiser {
         address winnerAddress;
     }
 
-    FundraiserDetails internal fundraiserDetails;
+    FundraiserDetails private fundraiserDetails;
     address[] userAddresses;
-    address internal winnerAddress;
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    address private cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
     
     constructor(
         address payable _organizer, 
@@ -47,28 +46,44 @@ contract Fundraiser {
         );
     }
 
-    function contribute (uint256 _amount) public payable {
-        require(fundraiserDetails.status, "Fundraiser has ended");
+
+    modifier checkIfEnded(){
+        require(fundraiserDetails.active, "Fundraiser has ended");
+        _;
+    }
+
+    /**
+        * @dev allow users to contribute to the fundraiser
+        * @param _amount the amount caller is contributing to the fundraise
+     */
+    function contribute (uint256 _amount) public payable checkIfEnded() {
+        require(_amount >= 1 ether, "Amount donated must be at least one CUSD");
+        require(fundraiserDetails.organizer != msg.sender, "You can't donate to your own campaign");
         IERC20Token(cUsdTokenAddress).transferFrom(
             msg.sender,
             address(this),
             _amount
         );
         fundraiserDetails.numberOfContributions++;
-        fundraiserDetails.amountRaised = fundraiserDetails.amountRaised + _amount;
+        uint newAmountRaised = fundraiserDetails.amountRaised + _amount;
+        fundraiserDetails.amountRaised = newAmountRaised;
         userAddresses.push(msg.sender);
-
-        if(fundraiserDetails.amountRaised >= fundraiserDetails.amountToBeRaised) {
-            randomlySelectWinner();
-            fundraiserDetails.status = false;
-            fundraiserDetails.winnerAddress = winnerAddress;
-        }
     }
 
-    function withdraw () public payable {
-        require(msg.sender == fundraiserDetails.organizer, 
-            "You don't have permission to use this function");
-        fundraiserDetails.status = false;
+    /**
+        * @dev allow the organizer of the fundraiser to withdraw the amount raised
+        * @notice this will end the fundraiser
+     */
+    function withdraw () public payable checkIfEnded() {
+        require(msg.sender == fundraiserDetails.organizer, "You don't have permission to use this function");
+        fundraiserDetails.active = false;
+
+        // selects a winner if amount to be raised has been exceeded
+        if(fundraiserDetails.amountRaised >= fundraiserDetails.amountToBeRaised){
+            randomlySelectWinner();
+    
+        }
+
         IERC20Token(cUsdTokenAddress).transfer(
             msg.sender,
             fundraiserDetails.amountRaised
@@ -91,7 +106,7 @@ contract Fundraiser {
             fundraiserDetails.prizeName,
             fundraiserDetails.prizeImage,
             fundraiserDetails.organizer,
-            fundraiserDetails.status,
+            fundraiserDetails.active,
             fundraiserDetails.amountToBeRaised,
             fundraiserDetails.numberOfContributions,
             fundraiserDetails.amountRaised, 
@@ -102,7 +117,7 @@ contract Fundraiser {
     function randomlySelectWinner () internal {
         uint256 randomNumber = uint256(blockhash(block.number - 1));
         uint256 winnerIndex = randomNumber % userAddresses.length;
-        winnerAddress = userAddresses[winnerIndex]; 
+        fundraiserDetails.winnerAddress = userAddresses[winnerIndex]; 
     }
 
 }
